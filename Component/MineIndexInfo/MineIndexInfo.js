69afb0e59d9b1733922d1565b0f31bb148f1d3f9
huhangbo <1746982699@qq.com>
2021-09-13 01:57:13 +0800

    改用grid布局

--- Component/MineIndexInfo/MineIndexInfo.js
+++ Component/MineIndexInfo/MineIndexInfo.js
@@ -3,6 +3,14 @@ Component({
     userInfo: {},
   },
   methods: {
-
+    toEdit: function () {
+      this.pageRouter.navigateTo({url: "/pages/mine/my/edit/edit"})
+    },
+    toUrl: function (e) {
+      const key = e.currentTarget.dataset.key
+      this.pageRouter.navigateTo({
+        url: `/pages/mine/my/${key}/${key}`
+      })
+    }
   }
 })
